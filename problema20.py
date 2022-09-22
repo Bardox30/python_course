51d1b6265147d81198c041967a821563d10623b8
Joel Ayllon <jal1984go@gmail.com>
2022-09-22 18:47:19 -0500

    problema20 dia 41 tipo 46

--- problema20.py
+++ problema20.py
@@ -1,12 +1,21 @@
-from turtle import position
-
 
 row1 = [" "," "," "]
 row2 = [" "," "," "]
 row3 = [" "," "," "]
 map = [row1,row2,row3]
 
+x = "X"
+
 print(f"{row1}\n{row2}\n{row3}")
 
-position = input("Where do you want to put the treasure? ")
+entrada = input("Where do you want to put the treasure? ")
 
+dab = entrada.split()
+y = dab[0]
+z = dab[1]
+numY = int(y)
+numZ = int(z)
+position = [numY,numZ]
+
+map[position[0]][position[1]] = x
+print(f"{map[0]}\n{map[1]}\n{map[2]}")
